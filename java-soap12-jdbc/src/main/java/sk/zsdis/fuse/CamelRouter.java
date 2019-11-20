ea841fd1f967b23039f72356ce3b7ec6d2d7142a
Fraser Walker <fwalker@redhat.com>
2019-11-20 07:55:32 +0200

    * Added spring boot application main class * Added camel route builder file

---
+++ java-soap12-jdbc/src/main/java/sk/zsdis/fuse/CamelRouter.java
@@ -0,0 +1,14 @@
+package sk.zsdis.fuse;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+
+}
