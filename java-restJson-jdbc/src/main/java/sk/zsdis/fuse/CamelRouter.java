37f0f7a13d5e4899beb153a9e79e431a6e93d2e0
Fraser Walker <fwalker@redhat.com>
2019-11-20 11:27:10 +0200

    * Added JSON Rest Consumer

--- java-restJson-jdbc/src/main/java/sk/zsdis/fuse/CamelRouter.java
+++ java-restJson-jdbc/src/main/java/sk/zsdis/fuse/CamelRouter.java
@@ -1,13 +1,36 @@
 package sk.zsdis.fuse;
 
+import org.apache.camel.builder.NoErrorHandlerBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MimeTypeUtils;
+import sk.zsdis.fuse.model.Request;
+import sk.zsdis.fuse.model.Result;
 
 @Component
 public class CamelRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        errorHandler(new NoErrorHandlerBuilder());
+        JacksonDataFormat jacksonDataFormat = new JacksonDataFormat(Request.class);
+        restConfiguration()
+                .component("servlet")
+                .bindingMode(RestBindingMode.json);
+
+        rest("/")
+                .post("/")
+                .consumes(MimeTypeUtils.APPLICATION_JSON_VALUE)
+                .produces(MimeTypeUtils.APPLICATION_JSON_VALUE)
+                .type(Request.class)
+                .route().routeId("query-json-only")
+                .to("log:" + CamelRouter.class.getCanonicalName() + "?level=WARN")
+                .to("direct:response").outputType(Result.class);
+
+        from("direct:response").routeId("response")
+                .transform().constant(new Result());
 
     }
 
